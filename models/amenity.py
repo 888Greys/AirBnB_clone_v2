ab3f1d2894f6f9ad294529df69ed52bc19706d84
888Greys <toxicgreys001@gmail.com>
2024-01-16 02:10:25 -0800

    Update

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        __tablename__: SQL table
+        name: input name
+        place_amenities: relationship to between Place & Amenity
+    """
+
+    __tabe__ = 'amenities':
+    name = Column(String(128), nullable=False)
