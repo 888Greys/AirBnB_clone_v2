d825376fc7bba796405d6b862e5221871b8f5937
888Greys <toxicgreys001@gmail.com>
2024-02-18 00:37:29 -0800

    Web stack

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-from flask import Flask
-app = Flask(__name__)
+"""A script that starts a flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+"""
 
-@app.route('/')
-def hello_world():
-    """ Print Hello HBNB"""
-    return 'Hello HBNB!'
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
